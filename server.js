2ef93911a9fa957effb1bef651d068551f8865db
carlosdevgit <chfla@live.com>
2026-07-29 17:39:23 -0300

    feat: habilita atualizacoes online automaticas no app churrasqueiro via server.url Vercel

--- server.js
+++ server.js
@@ -2627,6 +2627,7 @@ const noCacheHeaders = {
 app.use('/app-motoboy', express.static(path.join(__dirname, 'motoboy-app-nativo', 'www'), noCacheHeaders));
 app.use('/app-cozinha', express.static(path.join(__dirname, 'cozinha-app-nativo', 'www'), noCacheHeaders));
 app.use('/app-garcom', express.static(path.join(__dirname, 'garcom-app-nativo', 'www'), noCacheHeaders));
+app.use('/app-churrasqueiro', express.static(path.join(__dirname, 'churrasqueiro-app-nativo', 'www'), noCacheHeaders));
 app.get('/', (req, res) => res.redirect('/garcom'));
 app.get('/garcom', (req, res) => res.sendFile(path.join(__dirname, 'frontend', 'garcom', 'index.html')));
 app.get('/admin', (req, res) => res.sendFile(path.join(__dirname, 'frontend', 'admin', 'index.html')));
